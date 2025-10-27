2608dd9473bfde6c5ff147595c538dc8e96da19e
name1980 <petrovsd2002@gmail.com>
2025-10-27 15:20:02 +0300

    Added http interceptor, that logs response codes from server

--- core/src/main/kotlin/ru/dragontino/androidtestproject/core/di/CoreModule.kt
+++ core/src/main/kotlin/ru/dragontino/androidtestproject/core/di/CoreModule.kt
@@ -2,11 +2,13 @@ package ru.dragontino.androidtestproject.core.di
 
 import dagger.Module
 import dagger.Provides
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import ru.dragontino.androidtestproject.core.retrofit.RetrofitResources1
 import ru.dragontino.androidtestproject.core.retrofit.RetrofitResources2
 import ru.dragontino.androidtestproject.core.retrofit.RetrofitService1
 import ru.dragontino.androidtestproject.core.retrofit.RetrofitService2
+import ru.dragontino.androidtestproject.core.retrofit.ServerResponseInterceptor
 import javax.inject.Named
 import javax.inject.Singleton
 
@@ -28,18 +30,28 @@ class CoreModule {
     @Provides
     @Singleton
     @Named("server_1")
-    fun provideRetrofit1(): Retrofit {
+    fun provideRetrofit1(client: OkHttpClient): Retrofit {
         return Retrofit.Builder()
             .baseUrl(RetrofitResources1.BASE_URL)
+            .client(client)
             .build()
     }
 
     @Provides
     @Singleton
     @Named("server_2")
-    fun provideRetrofit2(): Retrofit {
+    fun provideRetrofit2(client: OkHttpClient): Retrofit {
         return Retrofit.Builder()
             .baseUrl(RetrofitResources2.BASE_URL)
+            .client(client)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addNetworkInterceptor(ServerResponseInterceptor())
             .build()
     }
 }
